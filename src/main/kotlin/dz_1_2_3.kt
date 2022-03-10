b69fd0ff7f40f3e41ad4deef4d6d76069f3ae762
VictorSev86 <butioxa_86@mail.ru>
2022-03-10 22:25:06 +0300

    Del variable discont

--- src/main/kotlin/dz_1_2_3.kt
+++ src/main/kotlin/dz_1_2_3.kt
@@ -3,22 +3,18 @@ fun main() {
     val regular = true
     val sum = 1000.0
     var newsum = sum
-    var discont = 0.0
     println("Покупка: " + sum + " руб.")
     if (lastsum > 1000 && lastsum <= 10000) {
-        discont = 100.0
-        newsum = newsum - discont
+        newsum -= 100
         println("После применения скидки 100 рублей: " + newsum + "руб.")
     }
     else if (lastsum > 10000) {
-        discont = newsum * 0.05
-        newsum = newsum - discont
+        newsum *= 0.95
         println("После применения скидки 5%: " + newsum + " руб.")
     }
 
     if (regular == true) {
-        discont = newsum * 0.01
-        newsum = newsum - discont
+        newsum *= 0.99
         println("После применения скидки 1%: " + newsum + " руб.")
     }
     println("Итоговая сумма покупки: " + newsum + " руб.")
